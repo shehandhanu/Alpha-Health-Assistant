20d7cf75d580cfdacace017a8a4899cd84c7aed7
lahiru <#L0718003955h>
2020-10-04 15:03:41 +0530

    Inputs adding to firebase, make firebase connection, some view function added. (after resolving errors)

--- app/src/main/java/com/example/AlphaHealthAssistant/ui/caloriecal/CaloriecalDailyReport.java
+++ app/src/main/java/com/example/AlphaHealthAssistant/ui/caloriecal/CaloriecalDailyReport.java
@@ -1,66 +1,42 @@
 package com.example.AlphaHealthAssistant.ui.caloriecal;
 
+import android.content.Intent;
 import android.os.Bundle;
 
-import androidx.fragment.app.Fragment;
-
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-
 import com.example.AlphaHealthAssistant.R;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link CaloriecalDailyReport#newInstance} factory method to
- * create an instance of this fragment.
- */
-public class CaloriecalDailyReport extends Fragment {
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+import android.view.View;
+import android.widget.Toast;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public CaloriecalDailyReport() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment CaloriecalDailyReport.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static CaloriecalDailyReport newInstance(String param1, String param2) {
-        CaloriecalDailyReport fragment = new CaloriecalDailyReport();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
+public class CaloriecalDailyReport extends AppCompatActivity {
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+        setContentView(R.layout.activity_caloriecal_daily_report);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_caloriecal_daily_report, container, false);
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+        Bundle bundle = getIntent().getExtras();
+        if (bundle != null) {
+            if (bundle.getString("some") != null) {
+                Toast.makeText(getApplicationContext(), "data" + bundle.getString("some"),
+                        Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 }
